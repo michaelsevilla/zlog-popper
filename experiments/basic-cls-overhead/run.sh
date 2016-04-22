639a07199de3cbc72016693bb0f04e05bb4870ac
Noah Watkins <noahwatkins@gmail.com>
2016-04-22 11:30:27 -0500

    expr: add new experiment
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- experiments/basic-cls-overhead/run.sh
+++ experiments/basic-cls-overhead/run.sh
@@ -4,13 +4,13 @@ reset=soft
 data_dev=sdc
 
 name=basic_cls_overhead
-runtime=120
+runtime=3600
 pg_nums="128"
 stripe_widths="128"
-queue_depths="16"
-entry_sizes="1024"
+queue_depths="32"
+entry_sizes="1024 4096"
 pool=zlog
-rest=60
+rest=120
 
 # workloads
 wl_n1="bytestream_n1_append"
@@ -23,7 +23,7 @@ workloads="$wl_n1"
 # cls_check_epoch: cls + omap-epoch-check + append
 # cls_check_epoch_hdr: cls + hdr-epoch-check + append
 #
-bytestream_n1_append_if="vanilla cls_no_index_wronly cls_check_epoch cls_check_epoch_hdr cls_full cls_full_hdr_idx"
+bytestream_n1_append_if="vanilla cls_no_index_wronly cls_check_epoch cls_full cls_full_hdr_idx"
 
 . $ZLOG_ROOT/docker/physical-design/runner.sh
 run_pd
