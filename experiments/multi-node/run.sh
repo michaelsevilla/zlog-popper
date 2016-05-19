35d291a38173fd748b977433bed9f09464454c34
Michael Sevilla <mikesevilla3@gmail.com>
2016-05-19 09:04:52 -0700

    expr: add cluster microbenchmarks
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/multi-node/run.sh
+++ experiments/multi-node/run.sh
@@ -2,6 +2,14 @@
 
 set -ex
 
-ANSIBLE_LOG_PATH="logs/ceph-zeqr.log" \
+#for i in 1 2 3 4 5 6 7 8 9 10; do
+#for i in 2; do
+#  ANSIBLE_LOG_PATH="logs/radosbench-${i}.log" \
+#  ansible-playbook --forks 50 --skip-tags "with_pkg" \
+#  -i inventory/cluster-${i}node -e inventory_name=cluster-${i}node \
+#  ceph.yml workloads/radosbench.yml
+#done
+
+ANSIBLE_LOG_PATH="logs/radosbench-${i}.log" \
 ansible-playbook --forks 50 --skip-tags "with_pkg" \
 ceph.yml workloads/bench-ng.yml
