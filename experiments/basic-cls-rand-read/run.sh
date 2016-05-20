8088830f20945d07bed069f7e9bf243ecfa66c0c
Noah Watkins <noahwatkins@gmail.com>
2016-05-20 09:01:10 -0700

    expr: add new expr
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- experiments/basic-cls-rand-read/run.sh
+++ experiments/basic-cls-rand-read/run.sh
@@ -1,28 +1,4 @@
 #!/bin/bash
 
-reset=soft
-data_dev=sdc
-
-name=basic_cls_rand_read
-runtime=3600
-pg_nums="128"
-stripe_widths="128"
-queue_depths="32"
-entry_sizes="4096"
-pool=zlog
-rest=120
-read_runtime=3600
-reads=yes
-
-# workloads
-wl_11="map_11 bytestream_11"
-wl_n1="map_n1 bytestream_n1_write"
-workloads="$wl_11 $wl_n1"
-
-# i/o interfaces
-map_n1_if="vanilla"
-bytestream_n1_write_if="vanilla"
-bytestream_n1_append_if="vanilla"
-
-. $ZLOG_ROOT/docker/physical-design/runner.sh
-run_pd
+./run1.sh
+./run2.sh
---
+++ experiments/basic-cls-rand-read/run1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+reset=soft
+data_dev=nvme0n1
+journal_dev=nvme1n1
+noop_dev="nvme0n1 --noop nvme1n1"
+
+name=librados_read_data_jrnl_nvme
+runtime=1800
+pg_nums="128"
+stripe_widths="128"
+queue_depths="32"
+entry_sizes="1024"
+pool=zlog
+rest=120
+read_runtime=1800
+reads=yes
+
+# workloads
+wl_11="map_11 bytestream_11"
+wl_n1="map_n1 bytestream_n1_write"
+workloads="$wl_11 $wl_n1"
+
+# i/o interfaces
+map_n1_if="vanilla"
+bytestream_n1_write_if="vanilla"
+bytestream_n1_append_if="vanilla"
+
+. $ZLOG_ROOT/docker/physical-design/runner.sh
+run_pd
